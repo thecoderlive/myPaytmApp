1669a6dfed3e67bbc87747a6b3dff90fc7b1e7b2
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-05-18 15:57:18 +0530

    Initial Commit for file: home_data.js

---
+++ home_data.js
@@ -0,0 +1,42 @@
+export const item = {
+    "logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/559.png",
+    "id": "51a0bd4e-32f0-46c6-8bac-9a44542b78ed",
+    "balance": "night",
+    "services": [
+        {
+            "service_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/718.png",
+            "id": "e0082dbc-87b2-4ba1-97c0-98206b912d50",
+            "service_name": "each"
+        },
+        {
+            "service_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/96.png",
+            "id": "3763693d-80bb-4b0d-9624-d682e6699279",
+            "service_name": "service"
+        },
+        {
+            "service_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/729.png",
+            "id": "d41fc1fc-fa2e-4e8b-a547-4be60a323008",
+            "service_name": "budget"
+        }
+    ],
+    "offers": [
+        {
+            "offer_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/725.png",
+            "id": "e0082dbc-87b2-4ba1-97c0-98206b912d50",
+            "offer_title": "capital",
+            "offer_description": "Stay her son school member sort research."
+        },
+        {
+            "offer_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/699.png",
+            "id": "3763693d-80bb-4b0d-9624-d682e6699279",
+            "offer_title": "develop",
+            "offer_description": "Husband remember under eight company positive"
+        },
+        {
+            "offer_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/23.png",
+            "id": "d41fc1fc-fa2e-4e8b-a547-4be60a323008",
+            "offer_title": "issue",
+            "offer_description": "Every own game southern big."
+        }
+    ]
+}

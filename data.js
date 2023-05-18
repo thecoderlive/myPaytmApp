c4bfd5971a2f9a5637efa549f6e8cdc7ab5ff8bb
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-05-18 15:57:19 +0530

    Initial Commit for file: data.js

---
+++ data.js
@@ -0,0 +1,143 @@
+export const data = {
+    "13fa2fb0-78f5-44e6-bcc9-1b38fc91d2a1": {
+        "id": "13fa2fb0-78f5-44e6-bcc9-1b38fc91d2a1",
+        "index": 0,
+        "created_at": "05/18/2023 15:55:15",
+        "created_by": ""
+    },
+    "51a0bd4e-32f0-46c6-8bac-9a44542b78ed": {
+        "id": "51a0bd4e-32f0-46c6-8bac-9a44542b78ed",
+        "parent_id": "13fa2fb0-78f5-44e6-bcc9-1b38fc91d2a1",
+        "index": 0,
+        "created_at": "05/18/2023 15:55:15",
+        "created_by": "",
+        "logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/559.png",
+        "balance": "night"
+    },
+    "e0082dbc-87b2-4ba1-97c0-98206b912d50": {
+        "id": "e0082dbc-87b2-4ba1-97c0-98206b912d50",
+        "parent_id": "51a0bd4e-32f0-46c6-8bac-9a44542b78ed",
+        "index": 0,
+        "created_at": "05/18/2023 15:55:15",
+        "created_by": "",
+        "service_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/718.png",
+        "service_name": "each",
+        "offer_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/725.png",
+        "offer_title": "capital",
+        "offer_description": "Stay her son school member sort research."
+    },
+    "3763693d-80bb-4b0d-9624-d682e6699279": {
+        "id": "3763693d-80bb-4b0d-9624-d682e6699279",
+        "parent_id": "51a0bd4e-32f0-46c6-8bac-9a44542b78ed",
+        "index": 0,
+        "created_at": "05/18/2023 15:55:15",
+        "created_by": "",
+        "service_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/96.png",
+        "service_name": "service",
+        "offer_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/699.png",
+        "offer_title": "develop",
+        "offer_description": "Husband remember under eight company positive"
+    },
+    "d41fc1fc-fa2e-4e8b-a547-4be60a323008": {
+        "id": "d41fc1fc-fa2e-4e8b-a547-4be60a323008",
+        "parent_id": "51a0bd4e-32f0-46c6-8bac-9a44542b78ed",
+        "index": 0,
+        "created_at": "05/18/2023 15:55:15",
+        "created_by": "",
+        "service_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/729.png",
+        "service_name": "budget",
+        "offer_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/23.png",
+        "offer_title": "issue",
+        "offer_description": "Every own game southern big."
+    },
+    "5bd19ec5-91e3-42e2-a6ca-58f39b7b8f34": {
+        "id": "5bd19ec5-91e3-42e2-a6ca-58f39b7b8f34",
+        "parent_id": "13fa2fb0-78f5-44e6-bcc9-1b38fc91d2a1",
+        "index": 0,
+        "created_at": "05/18/2023 15:55:15",
+        "created_by": "",
+        "logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/717.png",
+        "balance": "project"
+    },
+    "a565861b-d151-42ea-9d12-ed00b84bdf9f": {
+        "id": "a565861b-d151-42ea-9d12-ed00b84bdf9f",
+        "parent_id": "5bd19ec5-91e3-42e2-a6ca-58f39b7b8f34",
+        "index": 0,
+        "created_at": "05/18/2023 15:55:15",
+        "created_by": "",
+        "service_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/617.png",
+        "service_name": "although",
+        "offer_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/230.png",
+        "offer_title": "rule",
+        "offer_description": "Positive scene energy close admit third identify."
+    },
+    "13a2754b-7a84-46ff-92c1-99094c3ce1ec": {
+        "id": "13a2754b-7a84-46ff-92c1-99094c3ce1ec",
+        "parent_id": "5bd19ec5-91e3-42e2-a6ca-58f39b7b8f34",
+        "index": 0,
+        "created_at": "05/18/2023 15:55:15",
+        "created_by": "",
+        "service_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/336.png",
+        "service_name": "far",
+        "offer_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/153.png",
+        "offer_title": "scientist",
+        "offer_description": "National young her season"
+    },
+    "f99ca960-9b44-4656-b34a-cade18b03e8e": {
+        "id": "f99ca960-9b44-4656-b34a-cade18b03e8e",
+        "parent_id": "5bd19ec5-91e3-42e2-a6ca-58f39b7b8f34",
+        "index": 0,
+        "created_at": "05/18/2023 15:55:15",
+        "created_by": "",
+        "service_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/606.png",
+        "service_name": "about",
+        "offer_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/610.png",
+        "offer_title": "hundred",
+        "offer_description": "Home should medical increase space test system. Open notice"
+    },
+    "3f29cf98-2899-4873-b56c-50b3ff9ecb57": {
+        "id": "3f29cf98-2899-4873-b56c-50b3ff9ecb57",
+        "parent_id": "13fa2fb0-78f5-44e6-bcc9-1b38fc91d2a1",
+        "index": 0,
+        "created_at": "05/18/2023 15:55:15",
+        "created_by": "",
+        "logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/299.png",
+        "balance": "talk"
+    },
+    "8648c4f6-b947-46eb-a254-b8d52aa70d44": {
+        "id": "8648c4f6-b947-46eb-a254-b8d52aa70d44",
+        "parent_id": "3f29cf98-2899-4873-b56c-50b3ff9ecb57",
+        "index": 0,
+        "created_at": "05/18/2023 15:55:15",
+        "created_by": "",
+        "service_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/220.png",
+        "service_name": "large",
+        "offer_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/562.png",
+        "offer_title": "answer",
+        "offer_description": "Tax general from. Score"
+    },
+    "485abd11-a347-45e0-b5f2-114b39ac15c8": {
+        "id": "485abd11-a347-45e0-b5f2-114b39ac15c8",
+        "parent_id": "3f29cf98-2899-4873-b56c-50b3ff9ecb57",
+        "index": 0,
+        "created_at": "05/18/2023 15:55:15",
+        "created_by": "",
+        "service_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/510.png",
+        "service_name": "reveal",
+        "offer_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/317.png",
+        "offer_title": "role",
+        "offer_description": "Oil cut perhaps. Plant"
+    },
+    "e0ec6c50-f783-44c2-9f5d-dd81896d523b": {
+        "id": "e0ec6c50-f783-44c2-9f5d-dd81896d523b",
+        "parent_id": "3f29cf98-2899-4873-b56c-50b3ff9ecb57",
+        "index": 0,
+        "created_at": "05/18/2023 15:55:15",
+        "created_by": "",
+        "service_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/208.png",
+        "service_name": "mother",
+        "offer_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/419.png",
+        "offer_title": "the",
+        "offer_description": "Add recent cell nature. Resource real clearly police son"
+    }
+}
